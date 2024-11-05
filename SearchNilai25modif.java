3ed95a3db0fcc3955b01260878a492dd1c0ecf33
tugas <aldayanuar6@gmail.com>
2024-11-05 21:51:13 +0700

    modifikasi percobaan 4

---
+++ SearchNilai25modif.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SearchNilai25modif {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlahNilai = sc.nextInt();
+        int[] nilai = new int[jumlahNilai];
+
+        for (int i = 0; i < jumlahNilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int posisi = -1;
+
+        for (int i = 0; i < jumlahNilai; i++) {
+            if (nilai[i] == key) {
+                posisi = i;
+                break;
+            }
+        }
+
+        if (posisi != -1) {
+            System.out.println("Nilai ketemu " + key + ", merupakan nilai mahasiswa ke-" + (posisi + 1));
+           
+        }
+    }
+}
