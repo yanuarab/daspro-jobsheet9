7da7fe47c4876925b5b8314d12ec71855a97d5ad
tugas <aldayanuar6@gmail.com>
2024-11-05 19:04:34 +0700

    modifikasi percobaan 1

---
+++ ArrayBilangan25modif.java
@@ -0,0 +1,10 @@
+public class ArrayBilangan25modif {
+    public static void main(String[] args) {
+        int[] bil = {5, 13, -7, 17};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+        
+    }
+}
